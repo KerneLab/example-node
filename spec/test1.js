793979cf4e0f0fca0e438b44c588c438aff78d85
核心力量 <kernelforce@gmail.com>
2018-02-28 22:12:12 +0800

    complete test cases

--- spec/test1.js
+++ spec/test1.js
@@ -1,10 +1,7 @@
 var simple = require('../lib/script');
 
-describe("another example", function(){
-  it("should return true", function(){
-    expect(simple.math(1)).toBe(true);
-  });
-  it("should return false", function(){
-    expect(simple.math(0)).toBe(false);
+describe("another test", function(){
+  it("should return null", function(){
+    expect(simple.math(2)).toBe(null);
   });
 });
