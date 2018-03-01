08da66fb3bcb04eebdf5f262512084682110e769
核心力量 <kernelforce@gmail.com>
2018-03-01 21:51:19 +0800

    force fail

--- spec/test1.js
+++ spec/test1.js
@@ -4,4 +4,7 @@ describe("another test", function(){
   it("should return null", function(){
     expect(simple.math(2)).toBe(null);
   });
+  it("should be equal" , function(){
+  	expect([1,2]).toEqual([1,3]);
+  });
 });
